a920af1e786c9120b6acd6422439145ee1edd6f7
Zash60 <xath06@gmail.com>
2025-12-10 11:44:23 -0300

    Update Android.mk

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -1,56 +1,27 @@
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
+include $(CLEAR_ARS)
 
-# Kittymemory
-KITTYMEMORY_PATH = KittyMemory
-include $(CLEAR_VARS)
-LOCAL_MODULE    := Keystone
-LOCAL_SRC_FILES := $(KITTYMEMORY_PATH)/Deps/Keystone/libs-android/$(TARGET_ARCH_ABI)/libkeystone.a
-include $(PREBUILT_STATIC_LIBRARY)
+# Nome do arquivo final (.so)
+LOCAL_MODULE := ModMenu
 
-# Dobby
-include $(CLEAR_VARS)
-LOCAL_MODULE := Dobby
-LOCAL_SRC_FILES := Dobby/${TARGET_ARCH_ABI}/libdobby.a
-include $(PREBUILT_STATIC_LIBRARY)
-
-# Here is the name of your lib.
-# When you change the lib name, change also on System.loadLibrary("") under OnCreate method on StaticActivity.java
-# Both must have same name
-include $(CLEAR_VARS)
-LOCAL_MODULE    := MyLibName
-
-# -std=c++17 is required to support AIDE app with NDK
-LOCAL_CFLAGS := -w -s -Wno-error=format-security -fvisibility=hidden -fpermissive -fexceptions
-LOCAL_CPPFLAGS := -w -s -Wno-error=format-security -fvisibility=hidden -Werror -std=c++17
-LOCAL_CPPFLAGS += -Wno-error=c++11-narrowing -fpermissive -Wall -fexceptions
-LOCAL_LDFLAGS += -Wl,--gc-sections,--strip-all,-llog
-LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv2
-LOCAL_ARM_MODE := arm
-
+# --- ONDE PROCURAR OS ARQUIVOS (.h) ---
+# Aqui dizemos: "Olhe dentro destas pastas!"
 LOCAL_C_INCLUDES += $(LOCAL_PATH)
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/Includes/
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/Dobby/
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/KittyMemory
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/Menu
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/Includes
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/And64InlineHook
 
-# Here you add the cpp file to compile
+# --- QUAIS ARQUIVOS COMPILAR (.cpp) ---
+# Aqui listamos todos os arquivos de código
 LOCAL_SRC_FILES := Main.cpp \
-    Menu/Jni.cpp \
-    Menu/Menu.cpp \
-    Menu/Setup.cpp \
-    Includes/Utils.cpp \
-	Substrate/hde64.c \
-	Substrate/SubstrateDebug.cpp \
-	Substrate/SubstrateHook.cpp \
-	Substrate/SubstratePosixMemory.cpp \
-	Substrate/SymbolFinder.cpp \
-	KittyMemory/KittyArm64.cpp \
-    KittyMemory/KittyScanner.cpp \
-    KittyMemory/KittyMemory.cpp \
-    KittyMemory/KittyUtils.cpp \
-    KittyMemory/MemoryPatch.cpp \
-    KittyMemory/MemoryBackup.cpp \
-	And64InlineHook/And64InlineHook.cpp \
+                   KittyMemory/MemoryPatch.cpp \
+                   KittyMemory/Utils.cpp \
+                   Menu/Menu.cpp \
+                   Includes/Utils.cpp \
+                   Includes/Logger.cpp
 
-LOCAL_STATIC_LIBRARIES := Keystone Dobby
+# Bibliotecas do sistema Android necessárias
+LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv2
 
 include $(BUILD_SHARED_LIBRARY)
