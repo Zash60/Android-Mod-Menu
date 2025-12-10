bf9fc2fbf3016915b803e3d4a24841f59ea55825
Zash60 <xath06@gmail.com>
2025-12-10 11:41:17 -0300

    Update Application.mk

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,4 +1,5 @@
 APP_ABI := armeabi-v7a
 APP_PLATFORM := android-21
 APP_STL := c++_static
-APP_CPPFLAGS += -ffrtti -fexceptions
+# CORREÇÃO ABAIXO: Removido o 'f' extra de -ffrtti para -frtti
+APP_CPPFLAGS += -frtti -fexceptions
